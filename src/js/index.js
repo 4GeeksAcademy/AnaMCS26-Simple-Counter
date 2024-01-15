b611fff55da6e5a5283751174fff7b0651702cf5
AnaMCS26 <144897504+AnaMCS26@users.noreply.github.com>
2024-01-15 15:12:35 +0000

    primercontador

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,16 @@
 //import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
-
-// include your styles into the webpack bundle
+// import './icons.js';
+//include your styles into the webpack bundle
 import "../styles/index.css";
+import Home from "./component/home";
 
 //import your own components
-import Home from "./component/home.jsx";
+let seconds=0;
+setInterval(()=>{
+    seconds++;
+    ReactDOM.render(<Home seconds={seconds} />, document.querySelector("#app"));
+})
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
